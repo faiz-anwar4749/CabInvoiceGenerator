bf8b36a093bd17810b966d433b5479a61d57c240
Faiz <faiz.anwar4749@gmail.com>
2020-10-29 21:55:25 +0530

    [Faiz] Add: Added Welcome File

---
+++ CabInvoiceGenerator_TDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CabInvoiceGenerator_TDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Inovice Generator Program!");
+        }
+    }
+}
